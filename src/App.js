0e827444bab8fb0481dfdf538f3fe67f0355b6db
Andy Edwards <andyhedwards@gmail.com>
2018-09-23 18:20:40 +0100

    Add Footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import NavBar from './components/NavBar/NavBar';
 import AccountForm from './components/AccountForm/AccountForm';
+import Footer from './components/Footer/Footer';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <NavBar />
         <AccountForm />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = (props) => {
+  return (
+    <div id='footer'>
+      <div className='footer__totals'>
+        <p className='footer__subheading'>Total premium</p>
+        <p className='footer__total'>£223.50</p>
+        <p className='footer__subheading'>Per month. 0% APR, Tax Inc.</p>
+      </div>
+      <div className='footer__submit'>
+        <button className='footer__submit__button'>Confirm</button>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
