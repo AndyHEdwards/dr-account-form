59086472d56590e3a69ca946b22265f63ea8f9de
Andy Edwards <andyhedwards@gmail.com>
2018-09-23 17:37:28 +0100

    Add rest of account form

--- src/components/AccountForm/AccountForm.js
+++ src/components/AccountForm/AccountForm.js
@@ -32,6 +32,62 @@ const AccountForm = (props) => {
           className='account-form__input'
         />
       </div>
+
+      <div className='account-form__your-business'>
+        <div className='account-form__section-divider'>
+          <p className='account-form__section-divider__text'>Your business</p>
+          <div className='account-form__divider' />
+        </div>
+
+        <label className='account-form__label'>Enployee ERN</label>
+        <input
+          className='account-form__input'
+        />
+
+        <div className='account-form__checkbox'>
+          <input id='account-form__ern-checkbox' className='account-form__checkbox-input' type="checkbox" />
+          <span></span>
+          <label className='account-form__checkbox-label' htmlFor='account-form__ern-checkbox'>We're either exempt from having an ERN or we can provide it within 30 days</label>
+        </div>
+
+        <label className='account-form__label'>Premise address</label>
+        <textarea
+          className='account-form__input account-form__textarea'
+        />
+      </div>
+
+      <div className='account-form__your-policy'>
+        <div className='account-form__section-divider'>
+          <p className='account-form__section-divider__text'>Your policy</p>
+          <div className='account-form__divider' />
+        </div>
+
+        <label className='account-form__label'>Policy start date</label>
+        <input
+          className='account-form__input'
+          type='date'
+        />
+      </div>
+
+      <div className='account-form__your-account'>
+        <div className='account-form__section-divider'>
+          <p className='account-form__section-divider__text'>Your account</p>
+          <div className='account-form__divider' />
+        </div>
+
+        <label className='account-form__label'>Account password</label>
+        <input
+          className='account-form__input'
+          type='password'
+        />
+
+        <label className='account-form__label'>Confirm account password</label>
+        <input
+          className='account-form__input'
+          type='password'
+        />
+      </div>
+
     </div>
   );
 }
